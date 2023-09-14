4e53f2579bea19afdf658bbbe7728fd73849ea09
Brian Tum <brian.tum10@yahoo.com>
2023-09-14 19:50:55 +0300

    random algorithms

---
+++ randomAlgos/reverseString.go
@@ -0,0 +1,10 @@
+package randomalgos
+
+func Reverse(text string) string {
+	reversed := []rune(text)
+	for i, j := 0, len(reversed)-1; i < j; i, j = i+1, j-1 {
+		reversed[i], reversed[j] = reversed[j], reversed[i]
+	}
+
+	return string(reversed)
+}
---
+++ randomAlgos/reverseString_test.go
@@ -0,0 +1,21 @@
+package randomalgos
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	want := "nai"
+	got := Reverse("ian")
+
+	if strings.Compare(want, got) != 0 {
+		t.Errorf("wanted %s but got %s", want, got)
+	}
+}
+
+func FuzzReverse(f *testing.F) {
+	f.Fuzz(func(t *testing.T, a string) {
+		Reverse(a)
+	})
+}
